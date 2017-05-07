f7f5e59aa6adf8681424f9eec87c79a5fa636979
Disa <islamov.denis>
2017-05-07 15:59:58 +0300

    Add all new markers

--- Assets/ArCardsPrototype/Scripts/ImageTargetBehaviour/CustomTrackableEventHandler.cs
+++ Assets/ArCardsPrototype/Scripts/ImageTargetBehaviour/CustomTrackableEventHandler.cs
@@ -29,12 +29,22 @@ public class CustomTrackableEventHandler : MonoBehaviour,
     private float _elapsedTime = 0.0f;
     private bool _isRequiredReset;
 
+    public GameObject[] RandomElements;
+
     protected void Start()
     {
         _trackableBehaviour = GetComponent<TrackableBehaviour>();
         if (_trackableBehaviour)
         {
             _trackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+
+        if (RandomElements.Length > 0)
+        {
+            foreach (var re in RandomElements)
+            {
+                re.SetActive(false);
+            }
         }
     }
 
@@ -48,6 +58,14 @@ public class CustomTrackableEventHandler : MonoBehaviour,
             {
                 _elapsedTime = 0.0f;
                 _isRequiredReset = true;
+
+                if (RandomElements.Length > 0)
+                {
+                    foreach (var re in RandomElements)
+                    {
+                        re.SetActive(false);
+                    }
+                }
             }
         }
     }
@@ -70,6 +88,16 @@ public class CustomTrackableEventHandler : MonoBehaviour,
 
     private void TrackingFound()
     {
+        if (RandomElements.Length > 0 && _isRequiredReset)
+        {
+            var go = RandomElements[UnityEngine.Random.Range(0, RandomElements.Length)];
+            go.SetActive(true);
+
+            MainControllerTransform = go.transform.parent;
+            TargetAnimatorsParent   = go.transform.parent.gameObject;
+            PlaySoundRef = go.GetComponentInChildren<PlaySound>();
+        }
+
         Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
         //Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
 

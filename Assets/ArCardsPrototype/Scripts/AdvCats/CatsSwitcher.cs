59ada19a2bdd565afbce9a2b0b6c2647db4d4f12
islamov.denis <islamov.denis@gmail.com>
2017-05-22 16:23:37 +0300

    Add gyro cats

---
+++ Assets/ArCardsPrototype/Scripts/AdvCats/CatsSwitcher.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatsSwitcher : MonoBehaviour
+{
+    public GameObject[] ObjectList;
+    private int _index = 0;
+
+    public void Next()
+    {
+        ObjectList[_index].gameObject.SetActive(false);
+        _index++;
+        _index = _index % ObjectList.Length;
+        ObjectList[_index].gameObject.SetActive(false);
+    }
+
+    public void Prev()
+    {
+        ObjectList[_index].gameObject.SetActive(false);
+        _index--;
+        if (_index < 0)
+        {
+            _index = ObjectList.Length - 1;
+        }
+        ObjectList[_index].gameObject.SetActive(false);
+    }
+}
